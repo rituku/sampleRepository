bd34659806722e3843e3e7c2eb740d2b2cd9f48a
Ritu <rituku14@gmail.com>
2021-05-14 01:11:31 +0530

    Initial Commit C0

---
+++ gitMergeConflict/demoproject/src/com/Demo.java
@@ -0,0 +1,16 @@
+package com;
+
+public class Demo {
+	
+	public static void main(String[] args) {
+		
+		
+		int a = 2;
+		int b = 3;
+		
+		int c = a + b;
+		
+		System.out.println("output c1 == "+c);
+	}
+
+}
